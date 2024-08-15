236784d862250f9f6c8925336b5679c9cecc7693
Jon Robson <jdlrobson@gmail.com>
2024-08-15 16:56:14 +0100

    Ignore all non-main namespace pages and account for no results

--- src/map.js
+++ src/map.js
@@ -1,5 +1,5 @@
 let currentMap;
-const IGNORE_LIST = [ 'Main_Page', 'Special:Search' ];
+const IGNORE_LIST = [ 'Main_Page' ];
 import alpha from "./alpha2Lookup.json";
 
 const queries = {};
@@ -10,8 +10,9 @@ const mapIsoAlpha3ToAlpha2 = ( country ) => {
 
 function getTopArticleForCountry( year, country ) {
   if ( !queries[country] ) {
-    queries[country] = fetch(`https://wikimedia.org/api/rest_v1/metrics/pageviews/top-per-country/${mapIsoAlpha3ToAlpha2(country)}/all-access/${year}/01/01` )
+    queries[country] = fetch(`https://wikimedia.org/api/rest_v1/metrics/pageviews/top-per-country/${mapIsoAlpha3ToAlpha2(country)}/all-access/${year}/08/01` )
       .then((resp)=> {
+        console.log('got', resp, resp.status);
         if ( resp.status === 404 ) {
           return null;
         } else {
@@ -22,9 +23,14 @@ function getTopArticleForCountry( year, country ) {
   return queries[country]
     .then((result) => {
       if ( result ) {
-        const topArticle = result.items[0].articles.filter((a)=>!IGNORE_LIST.includes(a.article))[0];
-        topArticle.url = `https://${topArticle.project}.org/wiki/${topArticle.article}`;
-        return topArticle;
+        const topArticle = result.items[0].articles
+          .filter((a)=>!IGNORE_LIST.includes(a.article) && !a.article.includes(':'))[0];
+        if ( topArticle ) {
+          topArticle.url = `https://${topArticle.project}.org/wiki/${topArticle.article}`;
+          return topArticle;
+        } else {
+          return false;
+        }
       } else {
         return null;
       }
@@ -54,8 +60,10 @@ function initMap( project, year ) {
           let html;
           if ( topArticle === null ) {
             html = `Information for ${country} is not available per the Wikimedia Foundation Country and Territory Protection List.`;
+          } else if ( topArticle === false ) {
+            html = `There was not enough page views for ${country} (${geo.id}) on 1st August`;
           } else {
-            html = `The top article in ${country} on January 1st was
+            html = `The top article in ${country} on 1st August was
             <a href="${topArticle.url}">${topArticle.article.replace(/_/g, ' ')}</a>`;
           }
           document.getElementById('mapPopup').innerHTML = html;
