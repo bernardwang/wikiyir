166a66009f5dd2677b7121247e42b91ae4b12518
Jon Robson <jdlrobson@gmail.com>
2024-08-15 16:43:34 +0100

    Make the popup work correctly

--- src/map.js
+++ src/map.js
@@ -1,23 +1,34 @@
 let currentMap;
 const IGNORE_LIST = [ 'Main_Page', 'Special:Search' ];
+import alpha from "./alpha2Lookup.json";
+
 const queries = {};
 
 const mapIsoAlpha3ToAlpha2 = ( country ) => {
-  console.log('>>', country);
-  // TODO: Work out how to actually map alpha 2 to 3.
-  return country.replace(/(.)[AUIOE]/g, '$1');
+  return alpha[country];
 };
+
 function getTopArticleForCountry( year, country ) {
   if ( !queries[country] ) {
     queries[country] = fetch(`https://wikimedia.org/api/rest_v1/metrics/pageviews/top-per-country/${mapIsoAlpha3ToAlpha2(country)}/all-access/${year}/01/01` )
-      .then((resp)=>resp.json())
+      .then((resp)=> {
+        if ( resp.status === 404 ) {
+          return null;
+        } else {
+          return resp.json();
+        }
+      } )
   }
-  return queries[country].then((result) => {
-    console.log(result);
-    const topArticle = result.items[0].articles.filter((a)=>!IGNORE_LIST.includes(a.article))[0];
-    topArticle.url = `https://${topArticle.project}.org/wiki/${topArticle.article}`;
-    return topArticle;
-  });
+  return queries[country]
+    .then((result) => {
+      if ( result ) {
+        const topArticle = result.items[0].articles.filter((a)=>!IGNORE_LIST.includes(a.article))[0];
+        topArticle.url = `https://${topArticle.project}.org/wiki/${topArticle.article}`;
+        return topArticle;
+      } else {
+        return null;
+      }
+    });
 }
 function initMap( project, year ) {
   const mapElem = document.getElementById('map');
@@ -27,9 +38,11 @@ function initMap( project, year ) {
   const map = new Datamap({
     element: mapElem,
     done: function(datamap) {
-      datamap.svg.selectAll('.datamaps-subunit').on('click', function(geography) {
+      datamap.svg.selectAll('.datamaps-subunit').on('click', function(geo) {
           getTopArticleForCountry( year, geo.id ).then((topArticle) => {
-            window.open( topArticle.url );
+            if ( topArticle ) {
+              window.open( topArticle.url );
+            }
           } );
       });
     },
@@ -38,8 +51,14 @@ function initMap( project, year ) {
       popupTemplate: function(geo) {
         const country = geo.properties.name;
         getTopArticleForCountry( year, geo.id ).then((topArticle) => {
-          document.getElementById('mapPopup').innerHTML = `The top article in ${country} on January 1st was
-<a href="${topArticle.url}">${topArticle.article}</a>`;
+          let html;
+          if ( topArticle === null ) {
+            html = `Information for ${country} is not available per the Wikimedia Foundation Country and Territory Protection List.`;
+          } else {
+            html = `The top article in ${country} on January 1st was
+            <a href="${topArticle.url}">${topArticle.article.replace(/_/g, ' ')}</a>`;
+          }
+          document.getElementById('mapPopup').innerHTML = html;
         });
         return `<div id="mapPopup">${country}</div>`;
       }
